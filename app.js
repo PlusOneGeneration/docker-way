c8c0f4ca4396f4317636337a76d214ef79054784
slava hatnuke <slava.hatnuke@gmail.com>
2017-05-22 13:39:00 +0300

    hello

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('app started');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+
+app.listen(3000, function () {
+    console.log('app started on 3000');
+});
+
